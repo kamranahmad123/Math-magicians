e48ba9b2eff23a0311304319b00382e5749a54dd
kamranahmad123 <kamrankhan.ahmad2@yahoo.com>
2023-03-30 16:59:01 -0700

    create math magicians project events part

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,27 @@
-import CalculatorUI from './CalculatorUI';
+import { useState } from 'react';
+import calculate from '../logic/calculate';
+
+const entryObj = {
+  total: 0,
+  next: '',
+  operation: '',
+};
 
 export default function Calculator() {
+  const [vauleSet, setValueSet] = useState(entryObj);
+
+  const clickHandler = (event) => {
+    setValueSet(calculate(vauleSet, event.target.textContent));
+  };
   const dataArray = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
-  const calculatorButtons = dataArray.map((element) => <li className="button" key={element}>{element}</li>);
+  const calculatorButtons = dataArray.map((element) => <button className="button" key={element} type="button" onClick={clickHandler}>{element}</button>);
   return (
     <div className="Calculator-container">
-      <CalculatorUI />
+      <div dir="rtl" className="outPut">
+        {vauleSet.total}
+        {vauleSet.operation}
+        {vauleSet.next}
+      </div>
       <ul className="buttons">
         {calculatorButtons}
       </ul>
